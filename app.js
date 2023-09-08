27bbe18a09fd822a8a26f82dddd6f2e9b1e33801
dlrgus041 <dlrgus041@gmail.com>
2023-09-08 07:52:23 -0700

    stand-alone nodejs webserver

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import jade from 'jade';
+import { WebSocketServer } from 'ws';
+import { StringDecoder } from 'string_decoder';
         
-
 const app = express();
 
 app.use(express.static('public'));
@@ -13,4 +14,34 @@ app.listen(3000, () => {
 
 app.get('/hello', (req, res) => {
     res.render('hello');
+});
+
+const wss = new WebSocketServer({ port: 8080 });
+const decoder = new StringDecoder('utf8');
+
+const map = new Map();
+
+wss.on('connection', (ws) => {
+
+    function broadcast(data) {
+        for (const conn of map.keys()) {
+            conn.send(data);
+        }
+    }
+
+    ws.on('close', () => {
+        const name = map.get(ws);
+        map.delete(ws);
+        broadcast('Goodbye:' + name);
+    });
+  
+    ws.on('message', (rawdata) => {
+        const data = decoder.write(new Buffer(rawdata));
+        broadcast(data);
+
+        const arr = data.split(':');
+        if (arr[0] === 'Welcome') {
+            map.set(ws, arr[1]);
+        }
+    });
 });
--- public/main.js
+++ public/main.js
@@ -1,18 +1,11 @@
 var ws = null;
 var nickname = null;
+var isClientConnected = false;
 const chatArea = document.querySelector('#chatArea');
 
-function findNode(id) {
-    return document.getElementById(id);
-}
-
-function addBtnListener(id, listener) {
-    return findNode(id).addEventListener('click', listener);
-}
-
 function setEnablity(enablity, ...ids) {
-    for (const id in ids) {
-        findNode(`${id}`).disabled = !enablity;
+    for (const id of ids) {
+        document.querySelector(id).disabled = !enablity;
     }
 }
 
@@ -25,36 +18,75 @@ function chatAlert(type, message) {
     chatArea.appendChild(node);
 };
 
-addBtnListener('#connect', () => {
+function displayMessage(message, isMine) {
+
+    const inner = document.createElement('div');
+    inner.className = 'inner ' + (isMine ? 'mine' : 'other');
+    inner.style.display = 'inline';
+    inner.innerHTML = message;
+
+    const outer = document.createElement('div');
+    outer.style.textAlign = isMine ? 'right' : 'left';
+    outer.className = 'outer';
+    outer.appendChild(inner);
+
+    chatArea.appendChild(outer);
+};
+
+document.querySelector('#connect').addEventListener('click', () => {
     try {
-        ws = new WebSocket('ws://localhost:8080/server');
+        ws = new WebSocket('ws://localhost:8080');
+
+        ws.addEventListener('open', (event) => {
+            isClientConnected = true;
+            nickname = document.querySelector('#nickname').value;
+            setEnablity(false, '#nickname', '#connect');
+            setEnablity(true, '#trigger', '#message', '#send');
+            chatAlert('success', `Welcomme to Chat Server, ${nickname}!`);
+            ws.send('Welcome:' + nickname);
+        });
+
+        ws.addEventListener('close', (event) => {
+            if (!isClientConnected) return;
+            setEnablity(false, '#trigger', '#message', '#send');
+            chatAlert('danger', `Server terminated. See you next time, ${nickname}!`);
+        });
+
+        ws.addEventListener('message', (event) => {
+
+            const arr = event.data.split(':');
+            switch (arr[0]) {
+                case 'Welcome':
+                    chatAlert('success', `${arr[1]} joins the Chat Server. Say Hello to ${arr[1]}!`);
+                    break;
+                case 'Goodbye':
+                    chatAlert('warning', `${arr[1]} leaves the Chat Server.`);
+                    break;
+                default:
+                    displayMessage(event.data, arr[0] === nickname);
+            }
+        });
     } catch (e) {
         alert('Error occured whlie connect Server. \n\n' + e);
     }
 });
 
-addBtnListener('#close', () => {
+document.querySelector('#close').addEventListener('click', () => {
     try {
+        isClientConnected = false;
         ws.close();
         ws = null;
         nickname = null;
-        findNode('#nickname').value = '';
+        document.querySelector('#nickname').value = '';
         setEnablity(true, '#connect', '#nickname', '#connect');
         setEnablity(false, '#trigger', '#message', '#send');
-        chatAlert('warning', 'Connection Closed. You can Re-connect to Server.');
+        chatAlert('warning', 'Connection closed. You can re-connect to server.');
     } catch (e) {
         alert('Error occured whlie close connection. \n\n' + e);
     }
-})
-
-ws.addEventListener('open', (event) => {
-    nickname = findNode('#nickname').value;
-    setEnablity(false, '#nickname', '#connect');
-    setEnablity(true, '#trigger', '#message', '#send');
-    chatAlert('success', `Welcomme to Chat Server, ${nickname}!`);
 });
 
-ws.addEventListener('close', (event) => {
-    setEnablity(false, '#trigger', '#message', '#send');
-    chatAlert('danger', `Server Terminated. See you next time, ${nickname}!`);
+document.querySelector('#send').addEventListener('click', () => {
+    ws.send(nickname + ':' + document.querySelector('#message').value);
+    document.querySelector('#message').value = '';
 });
