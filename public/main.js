cc3a5c2e450b78dc0ac190231fa07a8f164a9ee0
dlrgus041 <dlrgus041@gmail.com>
2023-09-12 01:06:25 -0700

    temporary unactivate 'closeServer' action

--- public/main.js
+++ public/main.js
@@ -6,15 +6,14 @@ var isClientConnected = false;
 
 // html elements
 const groupChatArea = document.querySelector('#groupChatArea');
+const memberArea = document.querySelector('#memberArea');
+const chatRoomArea = document.querySelector('#chatRoomArea');
+
 const groupChatForm = document.querySelector('#groupChatForm');
 const alertForm = document.querySelector('#alertForm');
-
-const memberArea = document.querySelector('#memberArea');
 const memberForm = document.querySelector('#memberForm');
-
-const ChatRoomArea = document.querySelector('#ChatRoomArea');
-const ChatRoomForm = document.querySelector('#ChatRoomForm');
-
+const chatRoomForm = document.querySelector('#chatRoomForm');
+const modalForm = document.querySelector('#modalForm');
 
 // functions
 function displyAlert(type, message) {
@@ -53,17 +52,17 @@ function sendMessage(websocket, type, sender, receiver = null, message = null) {
     }));
 }
 
-function addMember(member) {
+function addMember(memberName) {
 
     const node = memberForm.cloneNode(true);
     node.style.display = 'block';
 
-    node.children[0].children[0].children[0].innerHTML = member;
+    node.children[0].children[0].children[0].innerHTML = memberName;
     node.children[0].children[1].children[0].addEventListener('click', () => {
-        sendMessage(ws, 'Invite', nickname, member);
+        sendMessage(ws, 'Invite', nickname, memberName);
     });
     node.children[0].children[1].children[1].addEventListener('click', () => {
-        sendMessage(ws, 'Whisper', nickname, member);
+        sendMessage(ws, 'Whisper', nickname, memberName);
     });
 
     memberArea.appendChild(node);
@@ -73,12 +72,79 @@ function clearMember() {
     memberArea.textContent = '';
 }
 
-function addChatRoom(chatRoom) {
+function addChatRoom(chatRoomName) {
 
+    const node = chatRoomForm.cloneNode(true);
+    node.style.display = 'block';
+
+    node.children[0].children[0].children[0].innerHTML = chatRoomName;
+    // node.children[0].children[1].children[0].addEventListener('click', () => {
+    //     sendMessage(ws, 'Invite', nickname, member);
+    // });
+    // node.children[0].children[1].children[1].addEventListener('click', () => {
+    //     sendMessage(ws, 'Whisper', nickname, member);
+    // });
+
+    chatRoomArea.appendChild(node);
+}
+
+function makeModal(type, receiver = '') {
+
+    clearModalForm();
+
+    var title = '';
+    var body = '';
+    var callback = () => {};
+
+    switch (type) {
+        case 'closeServer':
+            title = 'Disclamer';
+            body = 'Connection will be lost. Continue?';
+            callback = () => {
+                try {
+                    isClientConnected = false;
+                    ws.close(1000, nickname);
+                    ws = null;
+                    nickname = '';
+                    document.querySelector('#nickname').value = '';
+                    setEnablity(true, '#connect', '#nickname', '#connect');
+                    setEnablity(false, '#members', '#chatRooms', '#message', '#send');
+                    displyAlert('warning', nickname + ', Connection closed. You can re-connect to server.');
+                } catch (e) {
+                    console.log('Error occured whlie close connection. \n\n' + e);
+                }
+            };
+            break;
+        case 'whisper':
+            title = 'Notification';
+            body = `<h3>Whisper to ${receiver}:</h3><input id="whisperMessage" class="form-control ms-2 me-2" placeholder="Type message here">`;
+            callback = () => {
+                const message = document.querySelector('#whisperMessage').value;
+                sendMessage(ws, 'Whisper', nickname, receiver, message);
+            };
+            break;
+        case 'invite':
+            title = 'Notification';
+            body = `Invite ${receiver} to ChatRoom. Continue?`;
+            callback = () => {
+                sendMessage(ws, 'Invite', nickname, receiver);
+            };
+            break;
+        }
+
+    modalForm.children[0].children[0].children[0].children[0].innerHTML = title;
+    modalForm.children[0].children[0].children[1].innerHTML = body;
+    modalForm.children[0].children[0].children[2].children[0].addEventListener('click', callback);
 }
 
 // listeners
 document.querySelector('#connect').addEventListener('click', () => {
+
+    if (document.querySelector('#nickname').value === '') {
+        displyAlert('info', 'Please enter your nickname.');
+        return;
+    }
+
     try {
         ws = new WebSocket('ws://localhost:8080');
 
@@ -86,14 +152,15 @@ document.querySelector('#connect').addEventListener('click', () => {
             isClientConnected = true;
             nickname = document.querySelector('#nickname').value;
             setEnablity(false, '#nickname', '#connect');
-            setEnablity(true, '#members', '#chatRooms', '#trigger', '#message', '#send');
+            setEnablity(true, '#members', '#chatRooms', '#message', '#send');
             displyAlert('success', `Welcomme to Group Chat Server, ${nickname}! You can close connection to click 'Close' button.`);
             sendMessage(ws, 'Welcome', nickname);
+            console.log(ws);
         });
 
         ws.addEventListener('close', (event) => {
             if (!isClientConnected) return;
-            setEnablity(false, '#members', '#chatRooms', '#trigger', '#message', '#send');
+            setEnablity(false, '#members', '#chatRooms', '#message', '#send');
             displyAlert('danger', `Server terminated. See you next time, ${nickname}!`);
         });
 
@@ -124,20 +191,12 @@ document.querySelector('#connect').addEventListener('click', () => {
     }
 });
 
-document.querySelector('#close').addEventListener('click', () => {
-    try {
-        isClientConnected = false;
-        ws.close(1000, nickname);
-        ws = null;
-        nickname = '';
-        document.querySelector('#nickname').value = '';
-        setEnablity(true, '#connect', '#nickname', '#connect');
-        setEnablity(false, '#members', '#chatRooms', '#trigger', '#message', '#send');
-        displyAlert('warning', 'Connection closed. You can re-connect to server.');
-    } catch (e) {
-        alert('Error occured whlie close connection. \n\n' + e);
-    }
-});
+// TODO: have to fix close & re-open connection with server
+// document.querySelector('#closeServerTrigger').addEventListener('click', () => {
+//     makeModal('closeServer');
+//     const closeServerModal = new bootstrap.Modal(modalForm);
+//     closeServerModal.show();
+// });
 
 document.querySelector('#send').addEventListener('click', () => {
     sendMessage(ws, 'Message', nickname, null, document.querySelector('#message').value);
@@ -145,9 +204,13 @@ document.querySelector('#send').addEventListener('click', () => {
 });
 
 document.querySelector('#members').addEventListener('click', () => {
-    document.querySelector('#progress').style.display = 'block';
+    // document.querySelector('#progress').style.display = 'block';
     // sendMessage(ws, 'Members', nickname);
 });
 
 // initialize
 displyAlert('info', 'To join in group chat server, please type your nickname above and click "Connect" button.');
+addMember('John Doe');
+addMember('Jane Doe');
+addChatRoom('Chat Room 1');
+addChatRoom('Chat Room 2');
